c094cb191bb13bb4e4517f387d62165b6d7695a4
Rolake <morolaanney@gmail.com>
2025-03-16 21:09:01 +0000

    Updated structure

--- rbac.tf
+++ rbac.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault_access_policy" "function" {
-  for_each     = var.functions
+  for_each = var.functions
   key_vault_id = var.key_vault_id
   tenant_id    = var.tenant_id
 
@@ -21,29 +21,4 @@ resource "azurerm_role_assignment" "app_config_data_reader_func" {
   role_definition_name = "App Configuration Data Reader"
   principal_id         = azurerm_windows_function_app.new[each.key].identity[0].principal_id
 
-}
-
-resource "azurerm_key_vault_key" "key" {
-  name         = "${local.resource_name_prefix}-vault-key"
-  key_vault_id = azurerm_key_vault.functions_kv.id
-  key_type     = "RSA"
-  key_size     = 2048
-
-  key_opts = [
-    "decrypt",
-    "encrypt",
-    "sign",
-    "unwrapKey",
-    "verify",
-    "wrapKey",
-  ]
-
-  rotation_policy {
-    automatic {
-      time_before_expiry = "P30D"
-    }
-
-    expire_after         = "P90D"
-    notify_before_expiry = "P29D"
-  }
 }
