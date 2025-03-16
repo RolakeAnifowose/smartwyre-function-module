041d105f36274f8dc95fdacf33c1d700967b024c
Rolake <morolaanney@gmail.com>
2025-03-16 23:35:12 +0000

    Updated rbac.tf

--- rbac.tf
+++ rbac.tf
@@ -1,24 +1,18 @@
-resource "azurerm_key_vault_access_policy" "function" {
-  for_each     = var.functions
-  key_vault_id = var.key_vault_id
-  tenant_id    = var.tenant_id
-
-  object_id = azurerm_windows_function_app.new[each.key].identity[0].principal_id
-
-  secret_permissions = [
-    "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
-  ]
-
-  key_permissions = [
-    "Get", "List", "Update", "Create", "Import", "Delete", "Backup", "Recover", "Backup", "Restore"
-  ]
-}
-
 resource "azurerm_role_assignment" "app_config_data_reader_func" {
   for_each = var.functions
 
   scope                = var.app_config_id
   role_definition_name = "App Configuration Data Reader"
   principal_id         = azurerm_windows_function_app.new[each.key].identity[0].principal_id
+}
 
+resource "azurerm_key_vault_access_policy" "function" {
+for_each = var.functions
+  key_vault_id = var.key_vault_id
+  tenant_id    = var.tenant_id
+
+  object_id = azurerm_windows_function_app.new[each.key].identity[0].principal_id
+
+  secret_permissions = ["Get", "List"]
+  key_permissions    = ["Get", "List"]
 }
